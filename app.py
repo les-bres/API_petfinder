c563ce7723ca2e4b40c0cecc9fb89648fc4099a1
les-bres <les.bres.2@gmail.com>
2014-11-11 18:24:22 -0500

    made project

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ key = "0d7091936983bcf3195dc08f3ed4f7d4"
 #secret = "320445cae703c6d50a5aca4988cd3bc7"
 
 app = Flask(__name__)
-d = None
+#d = None
+app.secret_key = "hola"
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method=="GET":
         return render_template("home.html")
     else:
+        session.pop('url', None)
         url = "http://api.petfinder.com/pet.find?format=json&key=" + key
         url += "&location=" + request.form["zip"]
 
@@ -44,43 +46,77 @@ def home():
         for age in ages:
             if age != "":
                 url += "&age=" + age
-        
-        
+
         sizes = []
-        sizes.append( request.form["size1"])
-        sizes.append( request.form["size2"])
-        sizes.append( request.form["size3"])
-        sizes.append( request.form["size4"])
+        
+        try:
+            sizes.append( request.form["size1"])
+        except:
+            pass
+        try:
+            sizes.append( request.form["size2"])
+        except:
+            pass
+        try:
+            sizes.append( request.form["size3"])
+        except:
+            pass
+        try:
+            sizes.append( request.form["size4"])
+        except:
+            pass
 
         for size in sizes:
             if size != "":
                 url += "&size=" + size
+        
                 
-        print url
         req = urllib2.urlopen(url)
         d = json.loads( req.read() )
 
         # pets in list form
         pets = d["petfinder"]["pets"]["pet"]
+            
 
-        #session['pets'] = pets
-        #session['yo'] = "hi"
+        session['url'] = url
+        
     
-        #print session
+        print session
         return render_template("pets.html", pets=pets)
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
 
 @app.route("/pets/<pet>", methods = ["GET","POST"])
 def pet(pet=None):
-    
-    pets = d["petfinder"]["pets"]["pet"]
+
+    print escape(session['url'])
+    req = urllib2.urlopen( escape(session['url']))
+    d = json.loads( req.read() )
     pet_ = None
+
+    pets = d["petfinder"]["pets"]["pet"]
     for p in pets:
         if p['name']['$t'] == pet:
             pet_ = p
-            print "horray"
 
-            
-    return render_template("pet.html", pet=pet_)
+
+    try:
+        pic = pet_["media"]["photos"]["photo"] 
+        is_image = True
+    except:
+        is_image = False
+
+    contact = pet_["contact"]
+    contact2 = {}
+
+    for item in contact:
+        if contact[item].keys() != []:
+            contact2[item] = contact[item]
+
+
+    return render_template("pet.html", pet=pet_, is_image = is_image, contact=contact2)
 
 if __name__=="__main__":
     app.debug = True
