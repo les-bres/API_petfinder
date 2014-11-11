ab594156d592792f535e95a624d032f27b9284b4
Leslie Bresnahan <les.bres.2@gmail.com>
2014-11-11 18:40:47 -0500

    Delete petfinder.py

--- petfinder.py
+++
@@ -1,6 +0,0 @@
-import json, urllib2
-
-url = "http://api.petfinder.com/auth.getToken?key=0d8229854a6b11ffdbcbe87548b21c1b&sig=daae400291deb160215612dd7e1c025d"
-request = urllib2.urlopen(url)
-result = request.read()
-print result
